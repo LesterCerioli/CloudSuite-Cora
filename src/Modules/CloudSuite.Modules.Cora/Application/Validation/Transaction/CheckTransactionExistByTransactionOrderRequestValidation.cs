4311859f43043331fa48d28672d85845e290ea15
Igor Moreira <igor.moreira@lucastechnologyservice.com>
2023-11-01 14:19:46 -0300

    "feat(#1193): building transaction validations"

---
+++ src/Modules/CloudSuite.Modules.Cora/Application/Validation/Transaction/CheckTransactionExistByTransactionOrderRequestValidation.cs
@@ -0,0 +1,22 @@
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Transactions;
+using CloudSuite.Modules.Cora.Application.Handlers.Transactions.Requests;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Cora.Application.Validation.Transaction
+{
+    public class CheckTransactionExistByTransactionOrderRequestValidation : AbstractValidator<CheckTransactionExistByTransactionOrderRequest>
+    {
+
+        public CheckTransactionExistByTransactionOrderRequestValidation() 
+        {
+            RuleFor(a => a.TransactiOnorder)
+            .Matches(@"^\d+$")
+            .WithMessage("A ordem de transação deve conter apenas números. Letras e símbolos não são permitidos.");
+        }
+    }
+}
