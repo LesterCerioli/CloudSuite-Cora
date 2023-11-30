711ab07a9731d0728ea0b26f92ec7d1d6127240c
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-11-30 15:11:56 -0300

    feat(#1217): building handlers from transfer

--- src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Transfer/CreateTransferCommand.cs
+++ src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Transfer/CreateTransferCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Transfer.Responses;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Modules.Cora.Application.Handlers.Transfer
 {
-	internal class CreateTransferCommand
+	public class CreateTransferCommand : IRequest<CreateTransferResponse>
 	{
+		public Guid Id {  get; private set; }
 	}
 }
