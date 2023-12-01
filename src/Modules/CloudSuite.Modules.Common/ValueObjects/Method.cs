1c8c09a2055b1b7c27cb46bdafc3386764535070
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-12-01 01:46:26 -0300

    feat(#1280): Building Boleto Entity and Domain Contract

---
+++ src/Modules/CloudSuite.Modules.Common/ValueObjects/Method.cs
@@ -0,0 +1,22 @@
+﻿using NetDevPack.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Modules.Common.ValueObjects
+{
+	public class Method : ValueObject
+	{
+        public bool? PIX { get; set; }
+
+        // Payment by Bar-Code
+		public bool? BankSlip { get; set; }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
