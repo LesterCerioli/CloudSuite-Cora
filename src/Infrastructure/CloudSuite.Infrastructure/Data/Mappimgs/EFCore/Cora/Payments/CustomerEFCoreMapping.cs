fdba87722b25f644f730c78b4daaf4b2f8171a7f
Lester Cerioli <lester.cerioli@lucastechnologyservices.onmicrosoft.com>
2023-12-01 16:34:37 -0300

    feat(#1280): Building Handlers structure

---
+++ src/Infrastructure/CloudSuite.Infrastructure/Data/Mappimgs/EFCore/Cora/Payments/CustomerEFCoreMapping.cs
@@ -0,0 +1,19 @@
+﻿using CloudSuite.Modules.Cora.Domain.Models.Payments;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudSuite.Infrastructure.Data.Mappimgs.EFCore.Cora.Payments
+{
+	public class CustomerEFCoreMapping : IEntityTypeConfiguration<Customer>
+	{
+		public void Configure(EntityTypeBuilder<Customer> builder)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
