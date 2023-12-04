4c358dbdae4aced2609323e8c12b6eaaafa36372
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-12-04 15:54:59 -0300

    feat(#1217): refactoring handlers from account and customer

--- src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/Requests/CheckCustomerExistsByCnpjRequest.cs
+++ src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/Requests/CheckCustomerExistsByCnpjRequest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Customer.Responses;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Modules.Cora.Application.Handlers.Customer.Requests
 {
-	public class CheckCustomerExistsByCnpjRequest
+	public class CheckCustomerExistsByCnpjRequest : IRequest<CheckCustomerExistsByCnpjResponse>
 	{
-	}
+		public Guid Id { get; private set; }
+		public string? Cnpj {  get; set; }
+
+        public CheckCustomerExistsByCnpjRequest(string? cnpj)
+        {
+            Id = Guid.NewGuid();
+            Cnpj = cnpj;
+        }
+    }
 }
--- src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/Requests/CheckCustomerExistsBySocialReasonRequest.cs
+++ src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/Requests/CheckCustomerExistsBySocialReasonRequest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudSuite.Modules.Cora.Application.Handlers.Customer.Responses;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace CloudSuite.Modules.Cora.Application.Handlers.Customer.Requests
 {
-	public class CheckCustomerExistsBySocialReasonRequest
+	public class CheckCustomerExistsBySocialReasonRequest : IRequest<CheckCustomerExistsBySocialReasonResponse>
 	{
-	}
+		public Guid Id {  get; private set; }
+		public string? SocialReasion {  get; set; }
+
+        public CheckCustomerExistsBySocialReasonRequest(string? socialReasion)
+        {
+            Id = Guid.NewGuid();
+            SocialReasion = socialReasion;
+        }
+    }
 }
