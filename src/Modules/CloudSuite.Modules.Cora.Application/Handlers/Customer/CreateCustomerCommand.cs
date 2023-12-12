c212e24fab0cfbb43d7bf1cf1132c34360900961
Lucas Gabriel <lwkasgabriel@hotmail.com>
2023-12-12 19:56:46 -0300

    feat(#1217): finishing command from customer and fixing errors

--- src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/CreateCustomerCommand.cs
+++ src/Modules/CloudSuite.Modules.Cora.Application/Handlers/Customer/CreateCustomerCommand.cs
@@ -1,13 +1,32 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CloudSuite.Modules.Common.ValueObjects;
+using CloudSuite.Modules.Cora.Application.Handlers.Customer.Responses;
+using MediatR;
+using CustomerEntity = CloudSuite.Modules.Cora.Domain.Models.Payments.Customer;
 
 namespace CloudSuite.Modules.Cora.Application.Handlers.Customer
 {
-	public class CreateCustomerCommand
+	public class CreateCustomerCommand : IRequest<CreateCustomerResponse>
 	{
+		public Guid Id { get; private set; }
+		public string? Cnpj { get; set; }
+		public string? SocialReason { get; set; }
+		public string? ResponsibeContact { get; set; }
+		public string? TelephoneNumber { get; set; }
+		public string? TelephoneRegion { get; set;}
+
+		public CreateCustomerCommand()
+		{
+			Id  = Guid.NewGuid();
+		}
+
+		public CustomerEntity GetEntity()
+		{
+			return new CustomerEntity(
+				new Cnpj(Cnpj),
+				SocialReason,
+				ResponsibeContact,
+				new Telephone(TelephoneNumber, TelephoneRegion)
+				);
+		}
 	}
 }
